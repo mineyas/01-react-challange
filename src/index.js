3fdf54c1c5aad95d96668f6b10f1eb482e249fde
mineyas <yasmine-kharroubi_student2020@wilder.school>
2021-09-14 16:39:05 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.css';
+
+export const App = (
+  // write your HTML code here
+
+  <div>
+    <h1 className="text-center">My contacts list 📱</h1>
+
+    <div>
+      // add at least 5 people contact info (name, phone, email and a delete
+      button)
+    </div>
+  </div>
+);
+
+ReactDOM.render(App, document.getElementById('root'));
